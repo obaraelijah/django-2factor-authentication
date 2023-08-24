8392f447f84abf6d87d7c63607b094967e72e2da
obaraelijah <elijahobara357@gmail.com>
2023-08-24 11:36:45 +0300

    feat: add login, create and verifyotp views

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializers import (
+    VerifyOTPSerializer,
+    UserSerializer,
+    LoginSerialiazer,
+)
+
+
+class VerityOTPView(generics.GenericAPIView):
+    serializer_class = VerifyOTPSerializer
+
+    def post(self, request, format=None):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        login_info: dict = serializer.save()
+        return Response(login_info,status=200)
+    
+    
+class  UserProfileView(generics.GenericAPIView):
+    """Returns user profile details"""
+    permission_classes =  [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get(self, request, format=None):
+        user = self.request.user
+        serializer = self.serializer_class(user)
+        return Response({"success": True, "data": serializer.data}, status=200)
+    
+class LoginView(generics.GenericAPIView):
+    """Login with email and password"""
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerialiazer
+
+    def post(self, request, format=None):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user: User = serializer.save()
+        return Response(
+            {
+                "success": True,
+                "user": user.id,
+                "qr_code": user.qr_code.url,
+                "message": "Login Successful. Proceed to 2FA",
+            },
+            status=200,
+        )
+        
+        
+class CreateUserView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
+
+    def post(self, request, format=None):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {"success": True, "message": "Registration Successful!"}, status=200
+        )
